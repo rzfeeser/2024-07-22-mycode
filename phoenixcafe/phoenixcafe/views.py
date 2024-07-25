0a6912d624f83d2d77b56c6e3d7817479cc51a8f
rzfeeser <rzfeeser@example.com>
2024-07-25 19:33:11 +0000

    views and urls

---
+++ phoenixcafe/phoenixcafe/views.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# imports from Django
+from django.shortcuts import render
+from django.http import HttpResponse
+
+import datetime
+
+# This view will return "Welcome to the Phoenix Cafe!" as text
+def welcome(request):
+    return HttpResponse("Welcome to the Phoenix Cafe!")
+
+
+# your custom code will be here
+
+
+# NEW - new view that returns the current date and time
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)  # we are not returning a static string
+
